ff8b733c050f48f343615ca00db602d50749b19e
acefrost18 <65608310+acefrost18@users.noreply.github.com>
2024-12-09 21:26:28 -0500

    Update blackjack_interface.sol

--- contracts/blackjack_interface.sol
+++ contracts/blackjack_interface.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.8.26;
 interface BlackjackInterface {
 
     struct Hand {
-        uint[] hand; // An array of cards we are going to store the hands in this struct
+        uint[] hand; // An array of cards we are going to store the hands in this structure
     }
 
     /*
-    Insert comment here explaining code (Use msg.sender as the address of the player)
+    Check if requirements are met before playing
+    Take money out of playerBalance and store it into a variable and oficially starting the game
+    Add two cards into both the player's and dealer's hand with deal() function
+    Log the values
     */
     function startGame(uint betAmount) external;
 
@@ -18,39 +21,12 @@ interface BlackjackInterface {
     If playerHand < 21 call dealerAction()
     Else call BlackJackOrBust
     If action == stand call dealerAction()
-    */
-    function playerAction(bool hit) external;
-
-    /*
+    
+    DealerAction: Implemented in playerAction
     If dealerHandSum >= 17 call decideWinner()
     Else deal 1 card and add it to the value of dealerHand
     */
-
-
-    /*
-    Checks player's hand FIRST to see if handValue == 21 || handValue > 21   
-    Checks dealer's hand SECOND to see if handValue == 21 || handValue > 21   
-    If playerHandValue == 21 you win
-    If dealerHandValue == 21 Dealer win
-    Call endgame()
-    */
-   // function blackjackOrBust(uint handsum, string memory playerType) external;
-
-    /*
-    Compare dealerhandsum and playerhandsum. Whichever is higher wins. Set the winner variable = winner.
-    Edge case: both players hands are equal. In that case refund the bet amount. Set winner variable = tie.
-    Call endGame()
-    */
-    //function decideWinner() internal  ;
-
-    /*
-    Provide payout to winner. If the winner is player its 1.5*bet amount. 
-    If its the dealer the contract get the bet amount from the player if that already hasnt been done in start game.
-    Also if its a tie refund the bet amount to the player if they already paid at start of game.
-    */
-    //function endGame() internal;
-
-    //function calculateHandValue(uint[] memory hand) internal   view returns(uint); // function to calculate hand value
+    function playerAction(bool hit) external;
 
     event handValueUpdated(address indexed player, uint handValue); // log value of hand 
 
