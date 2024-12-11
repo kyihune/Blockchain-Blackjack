dc1b5ee1de9462b22ff72fc0b4526ccff772f085
SabeetHossain <66021093+SabeetHossain@users.noreply.github.com>
2024-12-11 16:57:12 -0500

    done with tests

--- contracts/blackjack.sol
+++ contracts/blackjack.sol
@@ -5,19 +5,80 @@ pragma solidity ^0.8.26;
 import "contracts/blackjack_interface.sol";
 
 
-contract Blackjack is BlackjackInterface { 
-    //testing commit from main machine
-    uint256 someNumber = 7;//used in the random number generation process.
+contract Blackjack is BlackjackInterface {
+    uint256 someNumber = 7;//used in the random number generation process. A code snippet online had it set to 0 but it can be anything.
     bool gameStarted = false; //for the start game function
-    bool isHit; //for player and dealer choice. false for stand, true for hit
-    bool isPlayer; //this is to pass on to blackjackorbust. if its true the player is calling. if its false its the dealer
     uint bet = 0; //bet amount
     uint win_lose_or_tie;// 0 is win for the player, 1 is lose for the player, 2 is tie.
+    //these 2 values are used for testing. going to leave these and certain related lines of code in other functions uncommented.
+    uint testPlayerHandValue;
+    uint testDealerHandValue;
+    //uncomment the variable below for testing.
+    //uint testcard = 0;
 
+/*
+ uncomment these functions if you want to run the tests.
+    function viewGameStarted() external view returns (bool) {
+        return gameStarted;
+    }
+
+    function viewDealtCard() external view returns (uint){
+        return testcard;
+    }
+
+    function testDealCard() external{
+        testcard = deal();
+    }
+
+    function viewPlayerHand() external view returns (uint){
+        return testPlayerHandValue;
+    }
+
+    function viewDealerHand() external view returns (uint){
+        return testDealerHandValue;
+    }
+
+   function testGameWinner(uint handValue, bool playerType) external  {
+        blackjackOrBust(handValue, playerType);
+    }
+
+    function viewContractBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function testDecideWinner(uint playerHandValue, uint dealerHandValue) external {
+        //copy and pasted from decide winner, removed some things that dont need to be tested.
+        if(playerHandValue == dealerHandValue) {
+            win_lose_or_tie = 2; // Tie
+        }else if (playerHandValue > dealerHandValue) {
+            win_lose_or_tie = 0; // Player wins
+        }else {
+            win_lose_or_tie = 1; // Dealer wins
+        }
+    }
+
+    function viewGameWinner() external view returns(uint)
+    {
+        return win_lose_or_tie;
+    }
+
+    function viewPlayerBalance() external view returns (uint)
+    {
+        return playerBalances[msg.sender];
+    }
+*/
+
+    function deposit() external payable {
+        require(msg.value > 0, "Deposit must be greater than 0.");
+        playerBalances[msg.sender] += msg.value;
+    }
 
     //internal because we only want the contract to be able to call this
     function deal() internal  returns (uint){
-        uint randomNumber = uint256(keccak256(abi.encodePacked(msg.sender, block.prevrandao,  someNumber, address(this))));
+        //note that while testing the deal function I noticed after multiple tests the same 5 cards were being dealt, so added a few more parameters.
+        //prevrandao and timestamp are bad cause the random number isnt truly random, miners and others can manipulate data of the timestamp or prevrandao
+        //to get more favorable results while using the contract.
+        uint randomNumber = uint256(keccak256(abi.encodePacked(msg.sender, block.prevrandao, msg.sender,block.timestamp,  someNumber, address(this))));
         uint card = (randomNumber%10)+1;
         someNumber += 1;
         return card;
@@ -41,13 +102,13 @@ contract Blackjack is BlackjackInterface {
         require(betAmount > 0, "Invalid Bet. Please bid over 0.");
 
         // Ensure the player has enough balance to start the game
-        require(playerBalances[msg.sender] >= betAmount * 2, "Insufficient balance to start the game. You need 2 times the amount you bet to play.");
+        require(playerBalances[msg.sender] >= betAmount, "Insufficient balance to start the game.");
 
         // Deduct the bet amount from the player's balance
         playerBalances[msg.sender] -= betAmount;
 
         // Store bet amount and set gameStarted to True
-        bet = betAmount;
+        bet = uint(betAmount);
         gameStarted = true;
 
         // initalize a hand for the dealer and the player; might need to add a delete for the hands just in case?
@@ -55,34 +116,41 @@ contract Blackjack is BlackjackInterface {
             playerHands[msg.sender].hand.push(deal());
             dealerHand.hand.push(deal());
         }
+        testPlayerHandValue = calculateHandValue(playerHands[msg.sender].hand); //new line added for testing
+        testDealerHandValue = calculateHandValue(dealerHand.hand); //new line added for testing
         uint playerHandValue = calculateHandValue(playerHands[msg.sender].hand);
         emit handValueUpdated(msg.sender,playerHandValue); // log the value of the players new hand value
     }
 
     /*
-    If action inputed == hit deal one card and add that to the value of the player hand 
+    If action inputed == hit deal one card and add that to the value of the player hand
     If playerHand < 21 call dealerAction()
     Else call blackJackOrBust
     If action == stand call dealerAction()
     */
-    function playerAction(bool hit) external { 
-        
+    function playerAction(bool hit) external {
+
         if(hit) { // if you hit you push and the value gets automatically updated
             playerHands[msg.sender].hand.push(deal()); // not going to decide win
             emit handValueUpdated(msg.sender, calculateHandValue(playerHands[msg.sender].hand)); // log value of the players new hand value again
         }
+
+        //whether the player hits or stands this new line stores the value of their hand for a test we made.
+        testPlayerHandValue = calculateHandValue(playerHands[msg.sender].hand); //new line added for testing
 
         if (calculateHandValue(playerHands[msg.sender].hand) >= 21) { // then you check either if you hit or if you stood if the value is >= 21 to go into the blackjackOrBust function
                 blackjackOrBust(calculateHandValue(playerHands[msg.sender].hand), true);
         }
 
         //if the player stands or hits and the dealer's hand is greater than 17 decide the winner
-        if(calculateHandValue(dealerHand.hand) >= 17) { 
+        if(calculateHandValue(dealerHand.hand) >= 17) {
+            testDealerHandValue = calculateHandValue(dealerHand.hand); //new line added for testing
             decideWinner();
         }
         //if it isn't greater than 17 give the dealer a card and calculate the value of the hand again
         else {
             dealerHand.hand.push(deal());
+            testDealerHandValue = calculateHandValue(dealerHand.hand); //new line added for testing
             //If the dealer's hand's value is less than 21 call decideWinner() and if it is greater or equal call blackjackorBust()
             if(calculateHandValue(dealerHand.hand) < 21) {
                 decideWinner();
@@ -93,25 +161,11 @@ contract Blackjack is BlackjackInterface {
         }
     }
 
-
-    
-
-    /*
-    Checks player's hand FIRST to see if handValue == 21 || handValue > 21   
-    Checks dealer's hand SECOND to see if handValue == 21 || handValue > 21   
-    If playerHandValue == 21 you win
-    If dealerHandValue == 21 dealer win
-    Call endgame()
-    */
-//    function blackjackOrBust(uint handSum, string memory playerType) external {
-        
-  //  }
-
     /*
     Prematurely ends the game in 2 cases:
     1. the player or the dealer gets blackjack.
     2. the player or dealer busts.
-    Only gets called during player or dealer action when the hand sum calculation is >=21 
+    Only gets called during player or dealer action when the hand sum calculation is >=21
     takes in the handSum(uint), and the type of player who the sum belongs to(player or dealer boolean)
     */
     function blackjackOrBust(uint handSum, bool playerType) internal {
@@ -123,24 +177,25 @@ contract Blackjack is BlackjackInterface {
         {
             win_lose_or_tie = 1; //dealer gets blackjack or player busts
         }
-        //i believe the correct function to call would be endGame but we'll have to make sure on that. for now its commented out.
+        /*I commented this out because playerAction goes through the whole loop. resulting in
+           multiple calls to endGame()
+        */
         //endGame()
     }
 
-    
+
     /*
     Compare dealerhandsum and playerhandsum. Whichever is higher wins.
-    Edge case: both players hands are equal. In that case refund the bet amount. 
+    Edge case: both players hands are equal. In that case refund the bet amount.
     Call endGame()
     */
-
     function decideWinner() internal {
         // Calculate the player's hand sum
         uint playerHandValue = calculateHandValue(playerHands[msg.sender].hand);
-      
+
          // Calculate the dealer's hand sum
         uint dealerHandValue = calculateHandValue(dealerHand.hand);
-      
+
         if (playerHandValue > 21) {
             win_lose_or_tie = 1; // Player busts, dealer wins
         } else if (dealerHandValue > 21) {
@@ -161,30 +216,29 @@ contract Blackjack is BlackjackInterface {
     }
 
     /*
-    Provide payout to winner. If the winner is player its 1.5*bet amount. 
+    Provide payout to winner. If the winner is player its 1.5*bet amount.
     If its the dealer the contract get the bet amount from the player if that already hasnt been done in start game.
     Also if its a tie refund the bet amount to the player if they already paid at start of game.
     */
     function endGame() internal   {
 
         // 0 represents a win for the player
-        if (win_lose_or_tie == 0 ){  
-            
+        if (win_lose_or_tie == 0 ){
+
             (bool sent, ) = payable(msg.sender).call{value : 2*bet}(""); // Avoiding warnings
-            require(sent, "Transfer failed.");// If sent is false, reports that the transfer has failed        
+            require(sent, "Transfer failed.");// If sent is false, reports that the transfer has failed
         }
         // 2 represents a tie for the player
         else if ( win_lose_or_tie ==  2 ){
 
             (bool sent, ) = payable(msg.sender).call{value : bet}("");
             require(sent, "Transfer failed.");
-        }    
+        }
 
     }
 
     //Calculates the value of the hand of player or dealer
-    
-    function calculateHandValue(uint[] memory hand)  internal pure    returns(uint) { // function to calculate hand value; might need to make a random number generator 1-10 
+    function calculateHandValue(uint[] memory hand)  internal pure    returns(uint) { // function to calculate hand value; might need to make a random number generator 1-10
         uint total = 0;
         for(uint i = 0; i < hand.length; i++) {
             total += hand[i];
